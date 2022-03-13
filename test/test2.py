d5f2693e40bec1e3c431f96d4b54b8eed6f4aa7b
Dan-Boat <dannboateng@gmail.com>
2022-03-13 19:05:23 +0100

    models class finish

--- test/test2.py
+++ test/test2.py
@@ -9,11 +9,12 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 sys.path.append("C:/Users/dboateng/Desktop/Python_scripts/ESD_Package")
 
 from Package.feature_selection import RecursiveFeatureElimination, TreeBasedSelection
 from Package.models import Regressors
-from Package.splitter import Splitter
+
 
 data_path = "C:/Users/dboateng/Desktop/Python_scripts/ESD_Package/data/"
 
@@ -36,6 +37,17 @@ selector.print_selected_features(df_X_train)
 print(selector.cv_test_score())
 train_X_new = selector.transform(df_X_train)
 test_X_new = selector.transform(df_X_test)
+
+def plot_style():
+    plt.style.use("bmh")
+    plt.rcParams['text.latex.preamble'] = [r"\usepackage{lmodern}"]
+    mpl.rc('text', usetex=True)
+    mpl.rc('font', size=18, family='serif')
+    mpl.rc('xtick', labelsize=22)
+    mpl.rc('ytick', labelsize=22)
+    mpl.rc('legend', fontsize=16)
+    mpl.rc('axes', labelsize=22)
+    mpl.rc('lines', linewidth=3)
 
 def plot_regression_results(ax, y_true, y_pred, title, scores, elapsed_time):
     """Scatter plot of the predicted vs true targets."""
@@ -62,19 +74,38 @@ def plot_regression_results(ax, y_true, y_pred, title, scores, elapsed_time):
     ax.set_title(title)
     
 # using the individual regressors
-models = ["LassoCV", "LassoLarsCV", "ARD", "BayesianRidge", "MLPRegressor", 
+models = ["LassoCV", "LassoLarsCV", "ARD", "BayesianRidge", 
           "RandomForest", "SGDRegressor", "ExtraTree", "RidgeCV"]
 
+plot_style()
+fig, axes = plt.subplots(2, 4, sharey=True, sharex = True, figsize=(23, 15))
+axs = np.ravel(axes)
 
+for i in range(len(models)):
+    import time 
+    start_time = time.time()
+    
+    regressor = Regressors(method= models[i], cv=10)
+    regressor.set_model()
+    regressor.fit(train_X_new, train_y)
+    
+    score = regressor.cross_validate(train_X_new, train_y)
+    
+    elapsed_time = time.time() - start_time
+    
+    y_pred = regressor.cross_val_predict(train_X_new, train_y)
+    
+    plot_regression_results(ax=axs[i], y_true=train_y, y_pred=y_pred, 
+                            title= models[i], 
+                            scores = (r"$R^2={:.2f} \pm {:.2f}$" + "\n" + r"$RMSE={:.2f} \pm {:.2f}$").format(
+            np.mean(score["test_r2"]),
+            np.std(score["test_r2"]),
+            -np.mean(score["test_neg_root_mean_squared_error"]),
+            np.std(score["test_neg_root_mean_squared_error"]),),
+                            elapsed_time=elapsed_time, )
+plt.suptitle("Base models performance")
+plt.tight_layout()
+plt.subplots_adjust(top=0.9)
+plt.savefig("models.svg", format= "svg", bbox_inches="tight", dpi=600)
+plt.show()
 
-regressor = Regressors(method=models[6], cv=10)
-regressor.set_model()
-regressor.fit(train_X_new, train_y)
-score = regressor.score(train_X_new, train_y)
-val = regressor.cross_validate(train_X_new, train_y)
-y_pred = regressor.cross_val_predict(train_X_new, train_y)
-
-print(np.mean(val["test_r2"]))
-print(np.std(val["test_r2"]))
-print(-np.mean(val["test_neg_root_mean_squared_error"]))
-print(np.std(val["test_neg_root_mean_squared_error"]))
