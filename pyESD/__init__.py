3015c56f29b70fd7f24e2d7e18422b192037574c
Dan-Boat <dannboateng@gmail.com>
2023-03-16 19:18:04 +0100

    pages

--- pyESD/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-PyESD: More information abou the package that would be part of the metadata
-"""
-
-__version__ = "0.1.0"
-__author__ = "Daniel Boateng"
-__credits__ = " Earth System Dynamics Research Group of University of Tuebingen"
